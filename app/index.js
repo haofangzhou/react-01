2e46041eca9b82fca8a0812a89ff1a456ed443a7
haofangzhou <haofangzhou@sinosoft.com.cn>
2017-08-08 13:28:09 +0800

    引入jquery

--- app/index.js
+++ app/index.js
@@ -1,13 +1,19 @@
 // 意思是从 lodash 里得到默认导出，并将默认导出命名为 _，这个 _ 可以换成任何一个其他的变量名。
 import _ from 'lodash'
+import $ from 'jquery'
+
 
 function component() {
-  var element = document.createElement('div');
+  // var element = document.createElement('div');
+  var element= $('<div></div>');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['hello','webpack'],''))
+  
 
-  return element;
+  // return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
